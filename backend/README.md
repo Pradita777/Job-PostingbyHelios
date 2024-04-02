5f1d1c573dce615c5080cf3c6eea2f83c264a1b6
Pradita777 <andres123prada@gmail.com>
2024-04-01 23:16:53 -0500

    Implementacion de openaiApi en el servidor flask

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+# Comando para activar el entorno virtual:
+    ```
+    .\venv\Scripts\activate
+    ```
+# Despues de acceder al entorno virtual debes descargar las dependencias del proyecto que se encuentran en requirements.txt y inicializar el proyecto de esta forma:
+    ```
+    flask run
+    ```
--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+import os
+import cv2
+import openai
+import numpy as np
+import base64
+import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for, redirect
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '¡Hola, mundo desde Flask!'
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/', methods=('POST','GET'))
+def index():
+    if request.method == 'POST':
+        prompt = request.form['text']
+        response = openai.images.generate(
+            model='dall-e-3',
+            prompt=prompt,
+            size='1024x1024',
+            quality='standard',
+            n=1,
+        )
+
+        image_url = response.data[0].url
+        image_data = requests.get(image_url).content
+        static_folder = "static"
+        image_path = os.path.join(static_folder, "siamese_cat.png")
+
+        with open(image_path, "wb") as f:
+            f.write(image_data)
+
+        return redirect(url_for("index"))
+    return render_template('index.html')
+
