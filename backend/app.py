fa7e8630628ded042ab7976d83f6d035de52421e
Tu nombre <alverth.antonio@hotmail.com>
2024-04-02 22:03:51 -0500

    Restructuración para la inversión de dependencias y separación de preocupaciones

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,23 @@
 import os
-import cv2
-import openai
-import numpy as np
-import base64
-import requests
-from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for, redirect
-
-load_dotenv()
+from modules.OpenAIGenerator import OpenAIGenerator
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+image_generator = OpenAIGenerator()
 
-@app.route('/', methods=('POST','GET'))
+
+@app.route('/', methods=('POST', 'GET'))
 def index():
     if request.method == 'POST':
         prompt = request.form['text']
-        response = openai.images.generate(
-            model='dall-e-3',
-            prompt=prompt,
-            size='1024x1024',
-            quality='standard',
-            n=1,
-        )
-
-        image_url = response.data[0].url
-        image_data = requests.get(image_url).content
+        image_url = image_generator.get_image_url(prompt)
         static_folder = "static"
-        image_path = os.path.join(static_folder, "siamese_cat.png")
-
-        with open(image_path, "wb") as f:
-            f.write(image_data)
-
+        image_path = os.path.join(static_folder, "generated_image.png")
+        image_generator.download_image(image_url, image_path)
         return redirect(url_for("index"))
     return render_template('index.html')
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
