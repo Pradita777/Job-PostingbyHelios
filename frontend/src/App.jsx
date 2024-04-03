fc9db644ab177dbbce8c04322895efa3711635b9
Julio Cesar <jcposadat@eafit.edu.co>
2024-04-02 21:04:53 -0500

    Primera implementación frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import magnetoLogo from '/magnetoLogoNobg.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://www.magneto365.com/es" target="_blank">
+          <img src={magnetoLogo} className="logo" alt="Magneto logo" />
+        </a>
+      </div>
+
+      <h1>Helios AI</h1>
+      
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
